8e7bd1dd49ad76bf5bc41c09b4d2970e6d2f2750
ncsfanbros2001 <ncssuperfan@gmail.com>
2024-09-14 09:26:33 +0700

    Add Repositories

---
+++ Mock_Project/Mock.Repository/Repositories/Repository/Classes/WishListDetailRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mock.Core.Data;
+using Mock.Core.Models;
+using Mock.Repository.Repositories.Generic;
+using Mock.Repository.Repositories.Repository.Interfaces;
+
+namespace Mock.Repository.Repositories.Repository.Classes
+{
+    public class WishListDetailRepository : GenericRepository<WishListDetails>, IWishListDetailRepository
+    {
+        private readonly LivebraryContext _context;
+        public WishListDetailRepository(LivebraryContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Mock_Project/Mock.Repository/Repositories/Repository/Interfaces/IGenreRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mock.Core.Models;
+using Mock.Repository.Repositories.Generic;
+
+namespace Mock.Repository.Repositories.Repository.Interfaces
+{
+    public interface IGenreRepository : IGenericRepository<Genre>
+    {
+    }
+}
