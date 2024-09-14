4d54d2cb4b4e4341cfd3189f620866deb7ba7c79
HaVietChien24 <chienhaviet2408@gmail.com>
2024-09-14 09:59:21 +0700

    Add Repositories

---
+++ Mock_Project/Mock.Core/Models/BorrowingDetails.cs
@@ -0,0 +1,18 @@
+
+namespace Mock.Core.Models
+{
+    public class BorrowingDetails
+    {
+        public int Id { get; set; }
+        public int BorrowingId { get; set; }
+        public Borrowing Borrowing { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+        public DateTime? ActualReturnDate { get; set; } //ngày trả thực tế
+        public decimal Penalty { get; set; } = 0;
+        public int NumberReturnedBook { get; set; } = 0;
+    }
+}
+
+
