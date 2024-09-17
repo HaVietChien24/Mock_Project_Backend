71c02f7cd98e62fd59d7f10b25b356e681d31701
maintt <nttmaii2407@gmail.com>
2024-09-18 02:38:32 +0700

    api add to wishlist

---
+++ Mock_Project/Mock.Bussiness/DTO/AddWishListDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Mock.Bussiness.DTO
+{
+    public class AddWishListDTO
+    {
+        public int userId { get; set; }
+        public int bookId { get; set; }
+    }
+}
--- Mock_Project/Mock.Bussiness/DTO/WishListDetailsDTO.cs
+++ Mock_Project/Mock.Bussiness/DTO/WishListDetailsDTO.cs
@@ -8,6 +8,7 @@ namespace Mock.Bussiness.DTO
         public int WishListId { get; set; }
         public int BookId { get; set; }
         public string BookTitle { get; set; }
+        public string ImageUrl { get; set; }
         public int? Quantity { get; set; }
     }
 }
