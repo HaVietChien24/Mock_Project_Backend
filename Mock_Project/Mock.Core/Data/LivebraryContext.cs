6a7db1b8df60fde6282bcdafe3f08804d7c9d838
HaVietChien24 <chienhaviet2408@gmail.com>
2024-09-17 20:34:37 +0700

    fix conflict

--- Mock_Project/Mock.Core/Data/LivebraryContext.cs
+++ Mock_Project/Mock.Core/Data/LivebraryContext.cs
@@ -19,15 +19,8 @@ namespace Mock.Core.Data
         public DbSet<User> Users { get; set; }
         public DbSet<WishList> WishLists { get; set; }
         public DbSet<WishListDetails> WishListDetails { get; set; }
-//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//        {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Server=LAPTOP-9LBBS3VQ;database=Mock_Project;uid=sa;password=1234;Integrated security=true;TrustServerCertificate=true");
-//            }
-//        }
 
+        public 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
