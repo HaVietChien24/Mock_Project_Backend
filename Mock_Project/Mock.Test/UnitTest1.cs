72c947570865371924f24d9335f1c21af53f5ceb
maintt <nttmaii2407@gmail.com>
2024-09-29 15:08:49 +0700

    add project test

---
+++ Mock_Project/Mock.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Mock.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Mock_Project/Mock.Test/WishListServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mock.Core.Data;
+using Mock.Core.Models;
+using Mock.Repository.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mock.Test
+{
+    public class WishListServiceTest
+    {
+        private WishListServiceTest wishListService;
+        [SetUp]
+        public void SetUp()
+        {
+          
+        }
+
+        [Test]
+        public void GetDTOByUserId_ReturnWishlistDTO()
+        {
+
+        }
+    }
+}
