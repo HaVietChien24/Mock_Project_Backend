0b4d189677387168e7da3a117f2a877661b5895c
ManhNguyen0801 <132865577+ManhNguyen0801@users.noreply.github.com>
2024-09-29 23:33:07 +0700

    pull code

--- Mock_Project/Mock.Core/Models/Book.cs
+++ Mock_Project/Mock.Core/Models/Book.cs
@@ -6,6 +6,7 @@ namespace Mock.Core.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+       
         public string Author { get; set; }
         public string Publisher { get; set; }
         public int PublishedYear { get; set; }
--- Mock_Project/Mock.Repository/Repositories/Generic/IGenericRepository.cs
+++ Mock_Project/Mock.Repository/Repositories/Generic/IGenericRepository.cs
@@ -3,10 +3,11 @@ namespace Mock.Repository.Repositories.Generic
 {
     public interface IGenericRepository<T> where T : class
     {
+        IList<T> GetAll(string includeProperties = null);
         T GetByID(int id, string includeProperties = null);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-        IList<T> GetAll(string includeProperties = null);
+     
     }
 }
